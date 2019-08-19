e0b0167d82bfc21ca11fede8d9243a13b9152e06
Robert Wilcox <42777835+stampylongr@users.noreply.github.com>
2019-08-19 15:08:28 -0400

    Update settings.py

--- closedverse/settings.py
+++ closedverse/settings.py
@@ -136,6 +136,7 @@ LOGIN_REDIRECT_URL = '/login/'
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.10/howto/static-files/
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static/'),
 ]
