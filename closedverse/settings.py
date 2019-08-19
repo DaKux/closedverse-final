ef73ff09c9fc598c0ed28882916e43b602fe33f0
Robert Wilcox <42777835+stampylongr@users.noreply.github.com>
2019-08-19 15:06:19 -0400

    Update settings.py

--- closedverse/settings.py
+++ closedverse/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/1.10/ref/settings/
 """
 
 import os
+import django_heroku
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -217,6 +218,5 @@ inactive_redirect = "https://www.youtube.com/watch?v=dQw4w9WgXcQ"
 # 0 - keep, 1 - move to 'rm' folder, 2 - DELETE
 image_delete_opt = 2
 
-# Configure Django App for Heroku.
-import django_heroku
+# Activate Django-Heroku.
 django_heroku.settings(locals())
